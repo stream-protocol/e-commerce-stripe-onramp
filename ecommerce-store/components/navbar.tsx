d392dc265684d1e98260b84d1c2ec7e1f0795427
StreamPay <105049869+stream-protocol@users.noreply.github.com>
2023-10-02 11:25:40 +0300

    fix

--- ecommerce-store/components/navbar.tsx
+++ ecommerce-store/components/navbar.tsx
@@ -12,7 +12,10 @@ const Navbar = async () => {
       <Container>
         <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center">
           <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
-            <p className="font-bold text-xl">StreamPayments™</p>
+            {/* Add the logo image here */}
+            <Image src="/public/streampay.svg" alt="StreamPay Logo" width={100} height={50} />
+            {/* You can adjust width and height according to your logo size */}
+            <p className="font-bold text-xl">Demo</p>
           </Link>
           <MainNav data={categories} />
           <NavbarActions />
