2bf74e3532f06c1e2020607e1a3fdfe0a0463ee3
StreamPay <105049869+stream-protocol@users.noreply.github.com>
2023-10-02 12:21:29 +0300

    Import the Image component

--- ecommerce-store/components/navbar.tsx
+++ ecommerce-store/components/navbar.tsx
@@ -1,37 +1,19 @@
 import Link from "next/link";
-import { useEffect, useState } from "react"; // Import useState for managing component state
-
+import Image from "next/image"; // Import the Image component
 import MainNav from "@/components/main-nav";
 import Container from "@/components/ui/container";
 import NavbarActions from "@/components/navbar-actions";
 import getCategories from "@/actions/get-categories";
 
-const Navbar = () => {
-  const [categories, setCategories] = useState([]); // Use useState to manage categories state
+const Navbar = async () => {
+  const categories = await getCategories();
 
-  useEffect(() => {
-    // Fetch categories when the component mounts
-    const fetchCategories = async () => {
-      try {
-        const data = await getCategories();
-        setCategories(data);
-      } catch (error) {
-        console.error("Error fetching categories:", error);
-      }
-    };
-
-    fetchCategories();
-  }, []);
-
-  return (
+  return ( 
     <div className="border-b">
       <Container>
         <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center">
-          <Link href="/">
-            {/* Wrap the link around the text for better accessibility */}
-            <a className="ml-4 flex lg:ml-0 gap-x-2">
-              <p className="font-bold text-xl">StreamPay™ Store</p>
-            </a>
+          <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
+            <p className="font-bold text-xl">StreamPay Store</p>
           </Link>
           <MainNav data={categories} />
           <NavbarActions />
@@ -40,5 +22,5 @@ const Navbar = () => {
     </div>
   );
 };
-
+ 
 export default Navbar;
